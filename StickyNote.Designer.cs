7fdf868cecbd7c4fba10dcff83b3f78c4ed88320
Jessica <jmtretter.jb@hotmail.com>
2022-06-13 15:25:36 -0500

    Add project files.

---
+++ StickyNote.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace StickyNotes
+{
+    partial class StickyNote
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(568, 122);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add Tab";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(568, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(543, 412);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(535, 384);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Notes";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(568, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 43);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Rename Selected Tab";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // StickyNote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "StickyNote";
+            this.Text = "Sticky Note";
+            this.Load += new System.EventHandler(this.StickyNote_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage4;
+        public System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ StickyNote.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StickyNotes
+{
+    public partial class StickyNote : Form
+    {
+        public StickyNote()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StickyNote_Load(object sender, EventArgs e)
+        {
+            string[] tabs = Directory.GetFiles("C:\\StickyNotes")
+                .Select(Path.GetFileName).ToArray();
+            if (tabs.Length > 0)
+            {
+                foreach (string tab in tabs)
+                {
+                    TabPage tp = new TabPage(tab);
+                    tabControl1.TabPages.Add(tp);
+
+                    TextBox tb = new TextBox();
+                    tb.Dock = DockStyle.Fill;
+                    tb.Multiline = true;
+
+                    tp.Controls.Add(tb);
+
+                    string text = " ";
+                    try
+                    {
+                        text = File.ReadAllText("C:\\StickyNotes\\" + tab);
+                    }
+                    catch
+                    {
+
+                    }
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        tb.Text = text;
+                    }
+                }
+
+            }
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            TabPage tp = new TabPage("New Tab");
+            tabControl1.TabPages.Add(tp);
+
+            TextBox tb = new TextBox();
+            tb.Dock = DockStyle.Fill;
+            tb.Multiline = true;
+
+            tp.Controls.Add(tb);
+
+            //TabRenameDialog dialog = new TabRenameDialog();
+            //dialog.Show();
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string[] tabs = Directory.GetFiles("C:\\StickyNotes", "*.txt")
+                .Select(Path.GetFileName).ToArray();
+
+            for (int i = 0; i < tabControl1.TabCount; i++)
+            {
+                string tabName = tabControl1.TabPages[i].Text;
+
+                TextWriter txt = new StreamWriter("C:\\StickyNotes\\" + tabName);
+                foreach (Control c in tabControl1.TabPages[i].Controls)
+                {
+                    txt.Write(c.Text);
+                    txt.Close();
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var showDialog = this.ShowDialog("Tab Name", "Rename the selected tab");
+            tabControl1.SelectedTab.Text = showDialog;
+        }
+
+        public string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form();
+            prompt.Width = 500;
+            prompt.Height = 150;
+            prompt.Text = caption;
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70 };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(textBox);
+            prompt.ShowDialog();
+            return textBox.Text;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
